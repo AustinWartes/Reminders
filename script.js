11f4065d48044be8b31ce2953af306dc5058b44b
AustinWartes <adwartes@gmail.com>
2026-07-28 16:44:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
 const reminderBox = document.getElementById("reminders");
-const dateTitle = document.getElementById("date");
 
 const today = new Date();
 
-const weekday = today.toLocaleDateString("en-US",{
-    weekday:"long"
+const weekday = today.toLocaleDateString("en-US", {
+    weekday: "long"
 });
-
-const fullDate = today.toLocaleDateString("en-US",{
-    weekday:"long",
-    month:"long",
-    day:"numeric"
-});
-
-dateTitle.textContent = fullDate;
 
 let found = false;
 
-reminders.forEach(reminder=>{
+reminders.forEach(reminder => {
 
-    if(reminder.type==="weekly" && reminder.weekday===weekday){
+    if (reminder.type === "weekly" && reminder.weekday === weekday) {
 
         reminderBox.innerHTML += `
             <div class="task">
@@ -28,16 +19,15 @@ reminders.forEach(reminder=>{
         `;
 
         found = true;
-
     }
 
 });
 
-if(!found){
+if (!found) {
 
-    reminderBox.innerHTML=`
+    reminderBox.innerHTML = `
         <div class="task">
-            ✅ Nothing scheduled today.
+            Nothing scheduled today
         </div>
     `;
 
